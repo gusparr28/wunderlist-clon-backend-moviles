05296df274aca9c3dedc4a98cee9d4851b54f960
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-10-30 17:29:24 -0400

    Configuring TypeScript and starting the server

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+// modules importation
+import express from 'express';
+const app = express();
+
+// middlewares
+
+// routes
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+export default app;
