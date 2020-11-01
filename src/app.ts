6b6490aa53ec0f2f2a773e6a9e72eaa8ae1c64f4
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-10-31 20:15:46 -0400

    Authentication done

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 // modules importation
 import express from 'express';
+import passport from 'passport';
+import passportMiddleware from './middlewares/passport';
 const app = express();
 
+import authRoutes from './routes/auth';
+import specialRoutes from './routes/special';
+
 // middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+passport.use(passportMiddleware);
 
 // routes
+app.use(authRoutes);
+app.use(specialRoutes);
 
 // settings
 app.set('port', process.env.PORT || 3000);
--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,13 @@
 import mongoose from 'mongoose';
+import { config as dotenv } from 'dotenv';
+dotenv();
 
-const MONGOURI = 'mongodb+srv://gusparr28:27284401g@practicing.xxeu5.mongodb.net/wunderlist-clon-moviles?retryWrites=true&w=majority';
+const MONGOURI: any = process.env.MONGOURI;
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true
-});
-
-const connection = mongoose.connection;
-
-connection.once('open', () => {
-    console.log('Successfully connected to MongoDB');
-});
+}).then(() => console.log('Successfully connected to MongoDB'))
+    .catch(err => console.log(err));
