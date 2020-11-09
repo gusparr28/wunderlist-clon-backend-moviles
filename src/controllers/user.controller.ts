d3bf32c88fbf5244418e77247d8c7792db652bd2
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-11-09 15:15:32 -0400

    Removing user functionalities

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,4 @@
 import { Response } from 'express';
-import { genSaltSync, hashSync } from 'bcryptjs';
 
 import User from '../models/User';
 
@@ -22,38 +21,4 @@ export const signOutUser = (req: any, res: Response, next: any) => {
         console.error(e);
         return res.status(500).json({ status: 500, message: 'Internal server error', error: e });
     };
-};
-
-export const updateUser = async (req: any, res: Response) => {
-    const { name, email, password } = req.body;
-    const { _id } = req.user;
-    try {
-        const savedUser = await User.findOne({ email, password });
-        if (savedUser) return res.status(422).json({
-            status: 422,
-            error: 'User already exists'
-        });
-        const salt = genSaltSync(10);
-        const hashedPassword = hashSync(password, salt);
-        const updatedUser = await User.findByIdAndUpdate(_id, {
-            name,
-            email,
-            password: hashedPassword
-        }, { new: true }).select("-password");
-        return res.status(200).json({ status: 200, message: 'User successfully updated', user: updatedUser })
-    } catch (e) {
-        console.error(e);
-        return res.status(500).json({ status: 500, message: 'Internal server error', error: e });
-    }
-};
-
-export const deleteUser = async (req: any, res: Response) => {
-    const { _id } = req.user;
-    try {
-        await User.findByIdAndDelete(_id);
-        return res.status(200).json({ status: 200, message: 'User successfully deleted' });
-    } catch (e) {
-        console.error(e);
-        return res.status(500).json({ status: 500, message: 'Internal server error', error: e });
-    }
 };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,12 +3,10 @@ const router = Router();
 
 import passport from 'passport';
 
-import { deleteUser, getUserInfo, signOutUser, updateUser } from '../controllers/user.controller';
+import { getUserInfo, signOutUser } from '../controllers/user.controller';
 
 router.route('/profile')
     .get(passport.authenticate('jwt', { session: false }), getUserInfo)
-    .put(passport.authenticate('jwt', { session: false }), updateUser)
-    .delete(passport.authenticate('jwt', { session: false }), deleteUser)
 
 router.route('/signout')
     .get(passport.authenticate('jwt', { session: false }), signOutUser)
