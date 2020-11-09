c5d6070f6bf0088580ac06d2e635235d46346dbc
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-11-09 18:36:43 -0400

    Heroku commit

--- src/utils/Strategies.ts
+++ src/utils/Strategies.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { config as dotenv } from 'dotenv';
 dotenv();
 
-const JWT_SECRET: any = process.env.JWT_SECRET; 
+const JWT_SECRET: any = process.env.JWT_SECRET || '28552455565g'; 
 
 export const createToken = (id: any) => {
     return jwt.sign({ id }, JWT_SECRET);
