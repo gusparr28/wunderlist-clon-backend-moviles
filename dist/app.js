4634401f735cfe3ec72b3b48bfcbfa889791a9ab
Gustavo Parra <gustavoparrabotero@gmail.com>
2020-11-09 22:05:59 -0400

    Updating dist files

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // modules importation
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const passport_1 = __importDefault(require("passport"));
+const cors_1 = __importDefault(require("cors"));
 const passport_2 = __importDefault(require("./middlewares/passport"));
 const app = express_1.default();
 const auth_1 = __importDefault(require("./routes/auth"));
@@ -15,9 +15,11 @@ const user_1 = __importDefault(require("./routes/user"));
 // middlewares
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-app.use(cors_1.default());
 app.use(passport_1.default.initialize());
 passport_1.default.use(passport_2.default);
+app.use(cors_1.default({
+    origin: true
+}));
 // routes
 app.use(auth_1.default);
 app.use(task_1.default);
